4fb3acbe4d9d49a41f13aebde9e4695c6d504bc6
Franklin <franklinryu@gmail.com>
2023-08-11 21:59:04 -0300

    primeiro commit

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,71 @@
+import { cardapio, itensExtras, combos, pagamentosValidos } from './dados/cardapio'
+
+class CaixaDaLanchonete {
+
+    calcularValorDaCompra(metodoDePagamento, itens) {
+
+        //verificar se o metodo de pagamento esta incluido no array pagamentosValidos
+        if (!pagamentosValidos.includes(metodoDePagamento)) {
+            return 'Forma de pagamento inválida!'
+        }
+
+        let valorDaCompra = 0
+        let carrinho = {}
+        let itensPrincipais = []
+
+        for (let informacoes of itens) {
+            let [codigo, quantidade] = informacoes.split(',')
+
+            let item = cardapio.find((item) => {
+                //retorna true se o código for igual ao código procurado
+                return item.codigo === codigo
+            })
+
+            if (!item) {
+                return 'Item inválido!'
+            }
+            //verifica se o código esta na lista de extras
+            if (codigo in itensExtras) {
+                //caso true, verifica se foi adicionado ao carrinho
+                if (!itensPrincipais.includes(itensExtras[codigo])) {
+                    return 'Item extra não pode ser pedido sem o principal'
+                }
+                //se nao estiver na lista de combos, adiciona na lista de principais
+            } else if (!combos.includes(codigo)) {
+                itensPrincipais.push(codigo)
+            }
+            //se o codigo nao esta no carrinho
+            if (!carrinho[codigo]) {
+                carrinho[codigo] = 0
+            }
+
+            carrinho[codigo] += quantidade
+            valorDaCompra += item.valor * quantidade
+        }
+
+        if (metodoDePagamento === 'dinheiro') {
+            valorDaCompra -= valorDaCompra * 0.05
+        } else if (metodoDePagamento === 'credito') {
+            valorDaCompra += valorDaCompra * 0.03
+
+        }
+        //verifica se o carrinho esta vazio a partir da lista de chaves do objeto
+        if (Object.keys(carrinho).length === 0) {
+            return 'Não há itens no carrinho de compra!'
+        }
+
+        if (valorDaCompra === 0) {
+            return 'Quantidade inválida!'
+        }
+
+        return `R$ ${valorDaCompra.toFixed(2).replace('.', ',')}`
+    }
+
+}
+
+export { CaixaDaLanchonete };
+
+
+//dinheiro -5%
+//credito +3%
+
---
+++ src/dados/cardapio.js
@@ -0,0 +1,21 @@
+const cardapio = [
+    { codigo: 'cafe', descricao: 'Café', valor: 3.00 },
+    { codigo: 'chantily', descricao: 'Chantily (extra do Café)', valor: 1.50 },
+    { codigo: 'suco', descricao: 'Suco Natural', valor: 6.20 },
+    { codigo: 'sanduiche', descricao: 'Sanduíche', valor: 6.50 },
+    { codigo: 'queijo', descricao: 'Queijo (extra do Sanduíche)', valor: 2.00 },
+    { codigo: 'salgado', descricao: 'Salgado', valor: 7.25 },
+    { codigo: 'combo1', descricao: '1 Suco e 1 Sanduíche', valor: 9.50 },
+    { codigo: 'combo2', descricao: '1 Café e 1 Sanduíche', valor: 7.50 }
+]
+
+const itensExtras = {
+    chantily: 'cafe',
+    queijo: 'sanduiche'
+}
+
+const combos = ['combo1', 'combo2']
+
+const pagamentosValidos = ['credito', 'debito', 'dinheiro']
+
+export { cardapio, itensExtras, combos, pagamentosValidos }
