4bff4b1b5e33a6161eec1ef701c9434e086012f7
Franklin <franklinryu@gmail.com>
2023-08-11 22:03:14 -0300

    reorganizando o codigo

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,13 +1,7 @@
 import { cardapio, itensExtras, combos, pagamentosValidos } from './dados/cardapio'
 
 class CaixaDaLanchonete {
-
     calcularValorDaCompra(metodoDePagamento, itens) {
-
-        //verificar se o metodo de pagamento esta incluido no array pagamentosValidos
-        if (!pagamentosValidos.includes(metodoDePagamento)) {
-            return 'Forma de pagamento inválida!'
-        }
 
         let valorDaCompra = 0
         let carrinho = {}
@@ -24,16 +18,20 @@ class CaixaDaLanchonete {
             if (!item) {
                 return 'Item inválido!'
             }
+
             //verifica se o código esta na lista de extras
             if (codigo in itensExtras) {
+
                 //caso true, verifica se foi adicionado ao carrinho
                 if (!itensPrincipais.includes(itensExtras[codigo])) {
                     return 'Item extra não pode ser pedido sem o principal'
                 }
+
                 //se nao estiver na lista de combos, adiciona na lista de principais
             } else if (!combos.includes(codigo)) {
                 itensPrincipais.push(codigo)
             }
+
             //se o codigo nao esta no carrinho
             if (!carrinho[codigo]) {
                 carrinho[codigo] = 0
@@ -43,12 +41,17 @@ class CaixaDaLanchonete {
             valorDaCompra += item.valor * quantidade
         }
 
+        //verificar se o metodo de pagamento esta incluido no array pagamentosValidos
+        if (!pagamentosValidos.includes(metodoDePagamento)) {
+            return 'Forma de pagamento inválida!'
+        }
+
         if (metodoDePagamento === 'dinheiro') {
             valorDaCompra -= valorDaCompra * 0.05
         } else if (metodoDePagamento === 'credito') {
             valorDaCompra += valorDaCompra * 0.03
-
         }
+
         //verifica se o carrinho esta vazio a partir da lista de chaves do objeto
         if (Object.keys(carrinho).length === 0) {
             return 'Não há itens no carrinho de compra!'
@@ -60,7 +63,6 @@ class CaixaDaLanchonete {
 
         return `R$ ${valorDaCompra.toFixed(2).replace('.', ',')}`
     }
-
 }
 
 export { CaixaDaLanchonete };
